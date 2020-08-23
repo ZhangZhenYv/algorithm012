6b3716b3f5a5a3dc70c970bcdaa22f2247f10c79
zhangzhenyv <qq2248865970@163.com>
2020-08-24 00:01:11 +0800

    week07 assiginment

---
+++ Week_06/src/main/java/day/MinimumTotal.java
@@ -0,0 +1,45 @@
+package api;
+
+/**
+ * 
+ * 给定一个三角形，找出自顶向下的最小路径和。每一步只能移动到下一行中相邻的结点上。
+ *
+ * 相邻的结点 在这里指的是 下标 与 上一层结点下标 相同或者等于 上一层结点下标 + 1 的两个结点。
+ *
+ *  
+ *
+ * 例如，给定三角形：
+ *
+ * [
+ *      [2],
+ *     [3,4],
+ *    [6,5,7],
+ *   [4,1,8,3]
+ * ]
+ * 自顶向下的最小路径和为 11（即，2 + 3 + 5 + 1 = 11）。
+ *
+ *  
+ *
+ */
+public class MinimumTotal {
+
+
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        int[] f = new int[n];
+        f[0] = triangle.get(0).get(0);
+        for (int i = 1; i < n; ++i) {
+            f[i] = f[i - 1] + triangle.get(i).get(i);
+            for (int j = i - 1; j > 0; --j) {
+                f[j] = Math.min(f[j - 1], f[j]) + triangle.get(i).get(j);
+            }
+            f[0] += triangle.get(i).get(0);
+        }
+        int minTotal = f[0];
+        for (int i = 1; i < n; ++i) {
+            minTotal = Math.min(minTotal, f[i]);
+        }
+        return minTotal;
+    }
+
+}
